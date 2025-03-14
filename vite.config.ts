411e091b25084180e85e60a867750c4ab16ba110
Andy Reid <andy@andyreid.net>
2025-03-14 00:17:53 -0500

    building example site

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,54 @@
-import { defineConfig } from 'vite'
-import { extname, relative, resolve } from 'path'
-import { fileURLToPath } from 'node:url'
-import { glob } from 'glob'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import { defineConfig } from 'vite';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    libInjectCss(),
-    dts({
-      tsconfigPath: resolve(__dirname, "tsconfig.lib.json"),
-    }),
-  ],
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
-      input: Object.fromEntries(
-        // https://rollupjs.org/configuration-options/#input
-        glob.sync('lib/**/*.{ts,tsx}', {
-          ignore: ["lib/**/*.d.ts"],
-        }).map(file => [
-          // 1. The name of the entry point
-          // lib/nested/foo.js becomes nested/foo
-          relative(
-            'lib',
-            file.slice(0, file.length - extname(file).length)
+export default defineConfig(({ mode }) => {
+  if (mode === 'lib') {
+    return {
+      plugins: [
+        react(),
+        libInjectCss(),
+        dts({
+          tsconfigPath: resolve(__dirname, 'tsconfig.lib.json'),
+        }),
+      ],
+      build: {
+        copyPublicDir: false,
+        lib: {
+          entry: resolve(__dirname, 'lib/main.ts'),
+          formats: ['es'],
+        },
+        rollupOptions: {
+          external: ['react', 'react/jsx-runtime'],
+          input: Object.fromEntries(
+            glob.sync('lib/**/*.{ts,tsx}', {
+              ignore: ['lib/**/*.d.ts'],
+            }).map((file) => [
+              relative(
+                'lib',
+                file.slice(0, file.length - extname(file).length)
+              ),
+              fileURLToPath(new URL(file, import.meta.url)),
+            ])
           ),
-          // 2. The absolute path to the entry file
-          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url))
-        ])
-      ),
-      output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].js',
-      }
-    }
+          output: {
+            assetFileNames: 'assets/[name][extname]',
+            entryFileNames: '[name].js',
+          },
+        },
+      },
+    };
+  } else {
+    // Demo build with index.html
+    return {
+      plugins: [react()],
+      build: {
+        outDir: 'dist', // Output separate from library
+      },
+    };
   }
-})
+});
